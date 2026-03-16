e1d210a8e78c5271505cf2d9d258f70e518c328d
Himesh Bhushan <himesh@Himeshs-MacBook-Air.local>
2026-03-17 01:15:00 +0800

    Signup Bug Fix

--- src/components/PanicButton.js
+++ src/components/PanicButton.js
@@ -45,6 +45,9 @@ const PanicButton = () => {
       >
         <img src={panicBtnImg} alt="Panic" /> 
       </button>
+      <div className="panic-bubble">
+        Panic Button
+      </div>
 
       {/* GLOBAL OVERLAY */}
       {panicMode && (
