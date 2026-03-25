d4b86eaf154ca60e6ac46bd611a44b9cc145e7a4
Himesh Bhushan <himesh@Himeshs-MacBook-Air.local>
2026-03-25 15:59:36 +0800

    bug: Back button in sharing page

--- src/pages/Sharing/Sharing.js
+++ src/pages/Sharing/Sharing.js
@@ -381,10 +381,6 @@ const Sharing = () => {
             /* --- 2. LEADERBOARD VIEW --- */
             <div className="leaderboard-wrapper">
               <div className="lb-header">
-                  <button className="lb-transparent-btn" onClick={() => setShowLeaderboard(false)}>
-                    <FiArrowLeft size={28} />
-                  </button>
-                  
                   <div className="lb-search-group">
                       <div className="lb-search-bar-alt" onClick={() => { setShowLeaderboard(false); setIsSearching(true); }}>
                         {t('find_add_friends')}
